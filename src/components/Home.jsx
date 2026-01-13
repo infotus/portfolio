20c37b6786f5935c991381b612caf5050a7b9362
infotus <eker.mem@gmail.com>
2026-01-13 11:44:27 +0900

    8+ years

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ const Main = () => {
                     </h1>
 
                     <h2  className="flex text-center sm:text-3xl text-xl pt-4 text-gray-100 p-2">
-                        7+ Years of Active Developer Experience <br />
+                        8+ Years of Active Developer Experience <br />
                         Specializing in Mobile Application Development
                     </h2>
                     <h2 className='flex sm:text-3xl text-xl pt-4 text-gray-100 p-4'>
